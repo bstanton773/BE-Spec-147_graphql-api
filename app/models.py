a86408e3eba663e1f94b0f29118f4f6d01d1ddc0
Brian <bstanton773@gmail.com>
2024-07-08 12:21:44 -0500

    Add __init__ method to User model to auto hash and save to db

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from werkzeug.security import generate_password_hash
 
 
 class Base(DeclarativeBase):
@@ -14,3 +15,13 @@ class User(db.Model):
     username: Mapped[str] = mapped_column(db.String(255))
     email: Mapped[str] = mapped_column(db.String(255))
     password: Mapped[str] = mapped_column(db.String(255))
+
+    def __init__(self, **kwargs):
+        # Call the parent __init__ so everything still works as intended
+        super().__init__(**kwargs)
+        # Set the password field to be the hashed version of the keyword password
+        self.password = generate_password_hash(kwargs['password'])
+        # Add the new instance to the database
+        db.session.add(self)
+        # Commit to the database
+        db.session.commit()
