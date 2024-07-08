abff24e8cccb6967dfd0f7d56981c40aeb6dd68a
Brian <bstanton773@gmail.com>
2024-07-08 16:02:56 -0500

    Add users to GraphQL QueryType

--- app/schema.py
+++ app/schema.py
@@ -1,15 +1,21 @@
 import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from app.models import User as UserModel
+from app import db
+
+
+class UserType(SQLAlchemyObjectType):
+    class Meta:
+        model = UserModel
+
 
 
 class Query(graphene.ObjectType):
-    test = graphene.String()
-    name = graphene.String()
+    users = graphene.List(UserType)
 
-    def resolve_test(root, info):
-        return 'This is a test string, how are you today!'
-
-    def resolve_name(root, info):
-        return 'Brian'
+    def resolve_users(root, info):
+        query = db.select(UserModel)
+        return db.session.scalars(query)
 
 
 schema = graphene.Schema(query=Query)
