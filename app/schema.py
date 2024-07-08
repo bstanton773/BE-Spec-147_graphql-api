4b4dc51b88a6847febd3b43f13b1a723c492217d
Brian <bstanton773@gmail.com>
2024-07-08 17:10:21 -0500

    Add posts field to QueryType

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
-from app.models import User as UserModel
+from app.models import User as UserModel, Post as PostModel
 from app import db
 
 
@@ -8,13 +8,20 @@ class UserType(SQLAlchemyObjectType):
     class Meta:
         model = UserModel
 
-
+class PostType(SQLAlchemyObjectType):
+    class Meta:
+        model = PostModel
 
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
+    posts = graphene.List(PostType)
 
     def resolve_users(root, info):
         query = db.select(UserModel)
+        return db.session.scalars(query)
+
+    def resolve_posts(root, info):
+        query = db.select(PostModel)
         return db.session.scalars(query)
 
 
